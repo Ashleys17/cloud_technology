dd0cf2c2fcd46835b7576a697d323aca80928ee6
Ashleys17 <143545337+Ashleys17@users.noreply.github.com>
2024-02-06 13:39:20 -0500

    Add files via upload

---
+++ Phyton Programming day 1/Ex1_output.py
@@ -0,0 +1,19 @@
+# Chap2 - Variables
+# Using print() function to display
+
+# The print function can display the result of evaluated expressions.
+print('Hello \n World')
+print("First day of Python programming")
+
+# The print function always display in a newline.
+print("Python")
+print("Programming")
+print("World")
+
+print("Ashley \n Sukhdeo")
+
+print(123)
+print (1+2+3)
+
+# A single print function can display several values.
+print (3+2, 3-2, 3*2)
